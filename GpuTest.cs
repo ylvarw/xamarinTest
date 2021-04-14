083ceb24ad484813f1fc3dae4e0dc55ec6798a0c
Ylva Rasmusson Wright <ylvarw@gmail.com>
2021-04-14 11:16:59 +0200

    rebuild with testpages

--- GpuTest.cs
+++ GpuTest.cs
@@ -1,4 +1,18 @@
-﻿namespace xamarinTest
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+using AndroidX.Core.View;
+using AndroidX.DrawerLayout.Widget;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Navigation;
+using Google.Android.Material.Snackbar;
+
+
+namespace xamarinTest
 {
 
 }
